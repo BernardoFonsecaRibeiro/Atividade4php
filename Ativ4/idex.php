b05eacccb9b117348932118c961546dd6b68269c
BernardoFonsecaRibeiro <164926105+BernardoFonsecaRibeiro@users.noreply.github.com>
2026-03-11 17:46:09 -0300

    Add files via upload

---
+++ Ativ4/idex.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Atividades</h1>
+    <ul>
+        <h2>Sequenciais</h2>
+        <li><a href="exer1.php">Exercício 1 - Conversão de Moeda</a></li>
+        <li><a href="exer2.php">Exercício 2 - Cálculo de Área e Perímetro</a></li>
+        <li><a href="exer3.php">Exercício 3 - Consumo de Combustível</a></li>
+        <h2>Comando If-Switch</h2>
+        <li><a href="exer4.php">Exercício 4 - Situação do Aluno </a></li>
+        <li><a href="exer5.php">Exercício 5 - Verificador de Idade</a></li>
+        <li><a href="exer6.php">Exercício 6 - Par ou Ímpar</a></li>
+        <li><a href="exer7.php">Exercício 7 - Dia da Semana</a></li>
+        <li><a href="exer8.php">Exercício 8 - Calculadora</a></li>
+        <li><a href="exer9.php">Exercício 9 - Mês por Extenso</a></li>
+        <h2>Comandos de Repetição</h2>
+        <li><a href="exer10.php">Exercício 10 - Fatorial de um Número</a></li>
+        <li><a href="exer11.php">Exercício 11 - Somatório de 1 a N</a></li>
+        <li><a href="exer12.php">Exercício 12 - Sequência de Pares (Intervalo)</a></li>
+        <h2>Arrays</h2>
+        <li><a href="exer13.php">Exercício 13 - Média de Vários Valores</a></li>
+        <li><a href="exer14.php">Exercício 14 - Lista de Compras (Checkboxes)</a></li>
+        <li><a href="exer15.php">Exercício 15 - Encontrar o Maior Valor</a></li>
+</body>
+</html>
